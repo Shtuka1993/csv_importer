7a389f7d98ad50d509bba291b3116a88a5c3ac4b
yshtokaylo <yshtokaylo@artelogic.net>
2020-06-23 20:01:01 +0300

    Create migrations for tables.

---
+++ database/migrations/2020_06_23_164005_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('manufacturer_id');
+            $table->bigInteger('subcategory_id');
+            $table->string('name');
+            $table->string('article');
+            $table->longText('description');
+            $table->integer('price');
+            $table->integer('guarantee');
+            $table->boolean('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
