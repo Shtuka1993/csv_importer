efae2b2afaa53a4dec2f406f726022a32599e550
yshtokaylo <yshtokaylo@artelogic.net>
2020-06-25 10:26:26 +0300

    Implemented basic functionality for CSV Uploading. Fixed namespaces for CSV module. Removed authentification from Form Request.

--- app/Http/FileController.php
+++ app/Http/FileController.php
@@ -3,33 +3,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Product;
-use Maatwebsite\Excel;
+use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Requests\TableFile;
-use App\Imports\ProductsImports;
+use App\Imports\ProductsImport;
 class FileController extends Controller {
     public function form(){
         return view('form');
     }
-    //public function importFileIntoDB(TableFile $request){
-    public function importFileIntoDB(Request $request){
+    public function importFileIntoDB(TableFile $request){
         if($request->hasFile('table_file')){
             $path = $request->file('table_file')->getRealPath();
-            //$file = $request->file('table_file');
-            //$data = Excel::load($path)->get();
-            //Excel::import(new ProductsImport,$file);
-            $array = Excel::toArray(new ProductsImport, $path);
-            var_dump($array);
-            //7Excel::import(new ProductsImport, $path);
-            /*if($data->count()){
-                foreach ($data as $key => $value) {
-                    $arr[] = ['name' => $value->name, 'details' => $value->details];
-                }
-                if(!empty($arr)){
-                    \DB::table('products')->insert($arr);
-                    dd('Insert Record successfully.');
-                }
-            }*/
+            Excel::import(new ProductsImport, $path);
         }
-        //dd('Request data does not have any files to import.');
     }
 }
