db41d37c51777598886f1efe9657c56742804420
yshtokaylo <yshtokaylo@artelogic.net>
2020-06-25 15:14:29 +0300

    Commented feltures catching.

--- app/Http/FileController.php
+++ app/Http/FileController.php
@@ -1,19 +1,37 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Product;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Requests\TableFile;
 use App\Imports\ProductsImport;
-class FileController extends Controller {
-    public function form(){
+
+class FileController extends Controller
+{
+    public function form()
+    {
         return view('form');
     }
-    public function importFileIntoDB(TableFile $request){
-        if($request->hasFile('table_file')){
+
+    public function importFileIntoDB(TableFile $request)
+    {
+        if ($request->hasFile('table_file')) {
             $path = $request->file('table_file')->getRealPath();
-            Excel::import(new ProductsImport, $path);
+            //try {
+                Excel::import(new ProductsImport, $path);
+            /*} catch (\Maatwebsite\Excel\Validators\ValidationException $e) {
+                $failures = $e->failures();
+
+                foreach ($failures as $failure) {
+                    $failure->row(); // row that went wrong
+                    $failure->attribute(); // either heading key (if using heading row concern) or column index
+                    $failure->errors(); // Actual error messages from Laravel validator
+                    $failure->values(); // The values of the row that has failed.
+                }
+            }*/
         }
     }
 }
--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -5,30 +5,88 @@ namespace App\Imports;
 use App\Product;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class ProductsImport implements ToModel,WithStartRow
+class ProductsImport implements ToModel, WithStartRow//, WithValidation
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return Product::firstOrCreate([
             'manufacturer_id' => 1,
             'subcategory_id' => 1,
 
-            'name'     => $row[4],
-            'article'    => $row[5],
-            'description'    => $row[6],
-            'price'    => $row[7],
-            'guarantee'    => ($row[8] || $row[8]=='Нет') ? 0 : $row[8],
-            'available'    => ($row[9]=='есть в наличие') ? true : false,
+            'name' => $row[4]??'--- NO NAME ---',
+            'article' => $row[5],
+            'description' => $row[6],
+            'price' => $row[7],
+            'guarantee' => (!$row[8] || $row[8] == 'Нет') ? 0 : $row[8],
+            'available' => ($row[9] == 'есть в наличие') ? true : false,
         ]);
     }
 
-    public function startRow(): int {
+    public function startRow(): int
+    {
         return 2;
     }
+
+    /*public function rules(): array
+    {
+        return [
+            '0' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('No category 1');
+                }
+            },
+            '1' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('No cutegory 2');
+                }
+            },
+            '2' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('No subcategory');
+                }
+            },
+            '3' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('No manufacturer');
+                }
+            },
+            '4' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('Name is empty');
+                }
+            },
+            '5' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('Article is empty');
+                }
+            },
+            '6' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('Description is empty');
+                }
+            },
+            '7' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('Price is empty');
+                }
+            },
+            '8' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('No guarantee');
+                }
+            },
+            '9' => function ($attribute, $value, $onFailure) {
+                if (empty($value)) {
+                    $onFailure('Not available');
+                }
+            },
+        ];
+    }*/
 }
